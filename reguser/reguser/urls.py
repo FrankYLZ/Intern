421bac2f74f26d08c269ece46f88ecb49366923c
FrankYLZ <zhouyalin0217@gmail.com>
2023-06-22 21:05:36 -0700

    update
    1. database connection
    2. adding reset_password and forget_password

--- reguser/reguser/urls.py
+++ reguser/reguser/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from reguser.views import Userregistration, UserLogin
+from reguser.views import Userregistration, UserLogin, PasswordReset, PasswordForgot
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 
@@ -24,6 +23,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('accounts/signup/',Userregistration, name='signup'),
     path('accounts/login/', UserLogin, name='login'),
+    path('accounts/reset/<uidb64>/<token>/', PasswordForgot, name='password_reset_confirm'),
+    path('accounts/reset_password_login/', PasswordReset, name='password_exists_reset'),
     path("accounts/", include("django.contrib.auth.urls")),
     path('', include('stocks.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home')
--- reguser/reguser/views.py
+++ reguser/reguser/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from reguser.models import Userreg
 from django.contrib import messages
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, views
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import admin
 from django.contrib.auth.models import User
@@ -55,3 +55,56 @@ def UserLogin(request):
                 return render(request,'registration/login.html')
     messages.success(request, "")
     return render(request,'registration/login.html')
+
+
+def PasswordReset(request, *args, **kwargs):
+    # temp = {'error': 0}
+    # context = {**temp, **kwargs}
+    context = {'error1': 0, 'error2':0, 'error3': 0}
+    if request.method == 'POST':
+        if request.POST.get('new_password1') and request.POST.get('new_password2') and request.POST.get('old_password'):
+            pass1 = request.POST['new_password1']
+            pass2 = request.POST['new_password2']
+            oldpass = request.POST['old_password']
+            if pass1 != pass2:
+                context['error1'] = 1
+                return render(request,'registration/password_exists_reset.html', context)
+            user = User.objects.get(username=request.user.username)
+            usersql = Userreg.objects.get(username=request.user.username)
+            if oldpass == pass1:
+                context['error3'] = 1
+                return render(request,'registration/password_exists_reset.html', context)
+            if usersql.password != oldpass:
+                context['error2'] = 1
+                return render(request,'registration/password_exists_reset.html', context)
+            user.set_password(pass1)
+            user.save()
+            usersql.password = pass1
+            usersql.save()
+            return render(request,'registration/password_reset_complete.html')
+    # validlink = True
+    return render(request,'registration/password_exists_reset.html', context)
+
+def PasswordForgot(request, *args, **kwargs):
+    # temp = {'error': 0}
+    # context = {**temp, **kwargs}
+    context = {'error': 0}
+    if request.method == 'POST':
+        if request.POST.get('new_password1') and request.POST.get('new_password2') and request.POST.get('email'):
+            pass1 = request.POST['new_password1']
+            pass2 = request.POST['new_password2']
+            mail = request.POST['email']
+            if pass1 != pass2:
+                context['error'] = 1
+                return render(request,'registration/password_reset_confirm.html', context)
+            user = User.objects.get(email=mail)
+            usersql = Userreg.objects.get(email=mail)
+            user.set_password(pass1)
+            user.save()
+            usersql.password = pass1
+            usersql.save()
+            return render(request,'registration/password_reset_complete.html')
+    # validlink = True
+    return render(request,'registration/password_reset_confirm.html', context)
+
+
